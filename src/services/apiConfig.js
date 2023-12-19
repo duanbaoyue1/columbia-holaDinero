4751cb72a5e82b8a2a2c2de3b02c791dc6f426b8
dby <13488796535@163.com>
2023-12-19 19:04:18 +0800

    fix: 修改apiconfig文件

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -2,14 +2,14 @@ const MODE = process.env.MODE;
 
 // api host
 const BASE_URL = {
-  development: "https://colombia-loan-test.6688-epoch.com",
+  development: "https://colombia-loan-test.timespodcast.cloud",
   production: "https://hzknbc.fun",
 };
 export const apiHost = BASE_URL[MODE];
 
 // upload host
 const UPLOAD_HOST = {
-  development: "http://abattoir-test.epoch-api.com",
+  development: "http://america-abattoir-test.timespodcast.cloud ",
   production: "https://jK7mXq3r.hzknbc.fun",
 };
 export const uploadHost = UPLOAD_HOST[MODE];
